d2c04a6f884a87a2059ba482a1297065529cf65e
Fancy Pants <edaily94@gmail.com>
2023-05-09 20:13:34 -0700

    adjusted some of the route codes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+const { Blogpost, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
-const { Blogpost, User, Comment } = require('../models');
+const { Blogpost, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogpostData = await Blogpost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+  
+        const blogPosts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+  
+        res.render('homepage', {
+            blogPosts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  });
